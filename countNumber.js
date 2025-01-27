993a98fb652eefa97e602462f4a3817dd027c27a
ajaynaveen99 <ajaynaveen337@gmail.com>
2025-01-27 19:57:13 +0530

    new

--- countNumber.js
+++ countNumber.js
@@ -1,29 +1,29 @@
-// function countNum(num){
-//     let count=0;
-//     if(num<0){
-//         num=-num;
-//     }
+function countNum(num){
+    let count=0;
+    if(num<0){
+        num=-num;
+    }
       
    
-//     while(num>0){
-//         count++;
-//         num=Math.floor(num/10);
+    while(num>0){
+        count++;
+        num=Math.floor(num/10);
         
-//     }
-//     if(count==0){
-//         return 1
-// }
-//     return count;
+    }
+    if(count==0){
+        return 1
+}
+    return count;
    
-//     }
+    }
    
-// console.log(countNum(19230));
-// console.log(countNum(0));
-// console.log(countNum(-2342319230));
+console.log(countNum(19230));
+console.log(countNum(0));
+console.log(countNum(-2342319230));
 
 //// type-2 using length
-function countNum(num){
-    console.log(num.toString(num).length())
-}
-countNum(932842);
+// function countNum(num){
+//     console.log(num.toString().length())
+// }
+// countNum(932842);
  
