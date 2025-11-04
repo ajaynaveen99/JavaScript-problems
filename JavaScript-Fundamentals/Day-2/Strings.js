a8e281c27fe20ca2caab50e0ea94512bc72fc3f2
ajaynaveen99 <ajaynaveen337@gmail.com>
2025-11-04 21:00:14 +0530

    new changes

--- JavaScript-Fundamentals/Day-2/Strings.js
+++ JavaScript-Fundamentals/Day-2/Strings.js
@@ -1,27 +1,53 @@
 //========1=============
 //-----without using inbuilt functions-----
-function rev(str){
-    let reversed="";
-    for(let i=str.length-1;i>=0;i--){
-       reversed=reversed+str[i];
-   
-    }
-     return reversed;
+function rev(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+
+  }
+  return reversed;
 }
 
 console.log(rev("Hello world"));
 
 
 ///-----using inbuilt functions-----
-function reverseStr(str){
-    return str.split("").reverse().join((""));
+function reverseStr(str) {
+  return str.split("").reverse().join((""));
 }
 console.log(reverseStr("Hello world"));
 //===============END======================
 
 
 //========2=============
-function capitalize(str){
+// without using inbuilt functions-----
+
+function firstLetterCaps(str){
+  let capital="";
+  let firstCharector=str[0];
+  let result="";
+  let lower = "abcdefghijklmnopqrstuvwxyz";
+  let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for(let i=0;i<lower.length-1;i++){
+    if(firstCharector==lower[i]){
+      capital=upper[i];
+      console.log(capital);
+      break;
+    }
+  } 
+  for(let j=1;j<str.length;j++){
+    result=result+str[j];
+    
+    }
+  return capital+result;
+
+}
+console.log(firstLetterCaps("ajaynaveen"));
+//=========================
+
+//==inbuilt function====
+function capitalize(str) {
   return str[0].toUpperCase() + str.slice(1);
 }
 //======end=========
@@ -45,10 +71,10 @@ function countVowels(str) {
 }
 console.log(countVowels("AjayNaveen"));
 //-----Vowel Count using inbuilt functions-----
-function countVowels(str){
+function countVowels(str) {
   let count = 0;
-  for(let char of str){
-    if("aeiouAEIOU".includes(char)){
+  for (let char of str) {
+    if ("aeiouAEIOU".includes(char)) {
       count++;
     }
   }
@@ -58,25 +84,58 @@ console.log(countVowels("AjayNaveen"));
 //=============end========================
 
 //==========4==============
-function isPalindrome(str){
+//-----Palindrome without using inbuilt functions-----
+function isPalindrome(str) {
   let reversed = str.split("").reverse().join("");
   return str === reversed;
 }
 console.log(isPalindrome("madam"));
+
 console.log(isPalindrome("hello"));
+//-----Palindrome without using inbuilt functions-----
+function reverse(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  } if (reversed == str) {
+    return "palindrome";
+
+  } else return "not a polindrome"
+}
+console.log(reverse("madam"));
+console.log(reverse("hello"));
 //============end===================
 //==========5=============
-function replaceSpaces(str){
+//=====Replace Spaces without using inbuilt functions=====
+function replaceSpaces(str) {
   return str.replaceAll(" ", "-");
 }
 console.log(replaceSpaces("Hello World from JavaScript"));
+//=====Replace Spaces using without in builtfunctions=====
+isSpace = (str) => {
+  let text = "";
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] == " ") {
+      text = text + "-";
+
+    }
+    else {
+      text = text + str[i]
+
+    }
+
+  } return text;
+
+}
+console.log(isSpace("Hello Guys well come to javaScript "));
+
 //============end===================
 //==========6=============
-function longestWord(sentence){
+function longestWord(sentence) {
   let words = sentence.split(" ");
   let longest = "";
-  for(let word of words){
-    if(word.length > longest.length){
+  for (let word of words) {
+    if (word.length > longest.length) {
       longest = word;
     }
   }
@@ -85,20 +144,20 @@ function longestWord(sentence){
 console.log(longestWord("hello world from JavaScript"));
 //============end===================
 //==========7=============
-function repeatString(str, n){
+function repeatString(str, n) {
   return str.repeat(n);
 }
 console.log(repeatString("abc", 3));
 //============end===================
 //==========8=============
-function contains(str, sub){
+function contains(str, sub) {
   return str.includes(sub);
 }
 console.log(contains("Hello world", "world"));
 console.log(contains("Hello world", "JavaScript"));
 //============end===================
 //==========9=============
-function titleCase(sentence){
+function titleCase(sentence) {
   return sentence
     .split(" ")
     .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
@@ -125,8 +184,8 @@ function removeNonAlpha(str) {
 console.log(removeNonAlpha("Hello, World! 123"));
 //============end===================
 //=== with inbuilt functions=====
-function removeNonAlpha(str){
-  return str.replace(/[^a-zA-Z]/g, ""); 
+function removeNonAlpha(str) {
+  return str.replace(/[^a-zA-Z]/g, "");
 }
 console.log(removeNonAlpha("Hello, World! 123"));
 //============end===================
