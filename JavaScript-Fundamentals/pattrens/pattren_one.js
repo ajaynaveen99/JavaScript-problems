b0bebd457da3b73ffccbcd2afb272602091a1dcd
ajaynaveen99 <ajaynaveen337@gmail.com>
2026-01-26 21:34:47 +0530

    piramid triangel added

---
+++ JavaScript-Fundamentals/pattrens/pattren_one.js
@@ -0,0 +1,19 @@
+function printPyramid(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let line = "";
+
+    // Add spaces
+    for (let j = 1; j <= rows - i; j++) {
+      line += " ";
+    }
+
+    // Add stars
+    for (let k = 1; k <= i; k++) {
+      line += "* ";
+    }
+
+    console.log(line);
+  }
+}
+
+printPyramid(5);
