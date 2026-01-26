81eb2ff00f968f7c6224e87c6ad66cb8e5b2a5d7
ajaynaveen99 <ajaynaveen337@gmail.com>
2026-01-26 23:59:15 +0530

    fix: correct indentation in printPyramid function

--- JavaScript-Fundamentals/pattrens/pattren_one.js
+++ JavaScript-Fundamentals/pattrens/pattren_one.js
@@ -1,7 +1,8 @@
 function printPyramid(rows) {
-  for (let i = 1; i <= rows; i++) {
-    let line = "";
 
+  for (let i = 1; i <= rows; i++) {
+
+        let line = "";
     // Add spaces
     for (let j = 1; j <= rows - i; j++) {
       line += " ";
