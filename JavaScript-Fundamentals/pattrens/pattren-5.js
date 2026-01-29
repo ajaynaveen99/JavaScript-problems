ea74a4b215d867df34c84268012b8e6ab8a3ae3e
ajaynaveen99 <ajaynaveen337@gmail.com>
2026-01-29 21:24:01 +0530

    pattren-5 updated

---
+++ JavaScript-Fundamentals/pattrens/pattren-5.js
@@ -0,0 +1,20 @@
+function NumPattrenFive(rows){
+    for(let i=1;i<=5;i++){
+        let num="";
+        for(let j=1;j<=i;j++){
+            num=num+j;
+        }
+        console.log(num)
+    }
+
+}
+NumPattrenFive(5)
+
+/* 
+Output
+1
+12
+123
+1234
+12345
+*/
