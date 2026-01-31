6103105eb8e56786803c907d6124e414be3e81f0
ajaynaveen99 <ajaynaveen337@gmail.com>
2026-01-31 09:59:05 +0530

    refactor pattern-9 implementation for better readability

--- JavaScript-Fundamentals/pattrens/pattren-9.js
+++ JavaScript-Fundamentals/pattrens/pattren-9.js
@@ -1,3 +1,23 @@
+function parttrenNine(rows){
+    for(let i=1;i<=rows;i++){
+        let star="";
+        for(let j=1;j<=rows-i;j++){
+            star+=" ";
+        }
+        for(let k=1;k<=2*i-1;k++){
+            if(i==1||i==rows){
+            star+="*"
+    }else if(k>=2&&k<=2*i-2){
+        star+=" ";
+        }else{
+            star+="*"
+        }  
+    }
+    console.log(star)
+
+    }
+}
+parttrenNine(5)
 /* Output
     *
    * *
