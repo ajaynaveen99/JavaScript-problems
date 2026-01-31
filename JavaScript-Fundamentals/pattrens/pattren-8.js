db8ff4e3b3aa5c71898d27be4c6fdafe939d5244
ajaynaveen99 <ajaynaveen337@gmail.com>
2026-01-31 09:23:03 +0530

    add pattern-8 implementation for inverted pyramid output

---
+++ JavaScript-Fundamentals/pattrens/pattren-8.js
@@ -0,0 +1,26 @@
+
+function PattrenEight(rows){
+
+    for(let i=rows;i>=1;i--){
+        star="";
+        for(j=0;j<rows-i;j++){
+            star+=" ";
+        }
+        for(k=1;k<=2*i-1;k++){
+            star+="*"
+        }
+        console.log(star);
+    }
+}
+PattrenEight(5)
+
+
+
+
+/* output
+*********
+ *******
+  *****
+   ***
+    *
+*/
