6c4bb0d838c4079e1a24a5bbef320f5e22354f78
ajaynaveen99 <ajaynaveen337@gmail.com>
2026-02-04 09:04:24 +0530

    pattren-15 updated

--- JavaScript-Fundamentals/pattrens/pattren-15.js
+++ JavaScript-Fundamentals/pattrens/pattren-15.js
@@ -1,12 +1,39 @@
+function pattrenFourteen(rows) {
+
+    for (let i = 1; i <= rows; i++) {
+        let star = "";
+        for (let j = 1; j < i; j++) {
+            star += " ";
+        }
+        for (let k = 1; k <= 9 - 2 * (i - 1); k++) {
+            star += "*";
+        }
+        console.log(star)
+    }
+    for (let i = rows - 1; i >= 1; i--) {
+        let star = "";
+        for (let j = 1; j < i; j++) {
+            star += " ";
+        }
+        for (let k = 1; k <= 9 - 2 * (i - 1); k++) {
+            star += "*";
+        }
+        console.log(star)
+    }
+
+
+}
+pattrenFourteen(5)
+
 /* Output
 *********
-*******
- *****
-  ***
-   *
-  ***
- *****
-*******
+ *******
+  *****
+   ***
+    *
+   ***
+  *****
+ *******
 *********
  
 */
